e42f0c53293e1494003bffe400007d108add0f03
NancyDLL <nano14@mac.com>
2017-07-24 10:59:04 -0500

    Added HTML CSS and JS from templates I built.

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//1 - Check connection to html page
+console.log("js for Basic Trivia game connected.")
+
+// * * * PSUEDOCODE & REQUIREMENTS * * *
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+// * * * ACTION ITEMS & TO D0 * * * 
+// 0.1 Create Repo and basic file structure.
+// 0.2 Create initial files - index, reset, and game.js
+// 0.3 Psuedocode off of watching the demo.
+// 1.0 Pick theme for the game.
+// 2.0 Select questions based on that theme.
+// 3.0 Do some basid styling on that theme.
+// 3.1 In building index.html think about divs for the JQuery to populate.
+// 4.0 Start to code the logic for JS, use console.log to validate as you build.
+//
